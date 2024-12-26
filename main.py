632065cfb32d80e58a0771ca383308b9bab39a45
M Power <147989511+mPower-codes@users.noreply.github.com>
2024-12-26 10:02:37 +0100

    Aktualisiere pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ def on_sound_loud():
     basic.clear_screen()
 input.on_sound(DetectedSound.LOUD, on_sound_loud)
 
+def on_logo_touched():
+    basic.show_number(input.light_level())
+input.on_logo_event(TouchButtonEvent.TOUCHED, on_logo_touched)
+
 def on_button_pressed_a():
     music.play(music.tone_playable(698, music.beat(BeatFraction.WHOLE)),
         music.PlaybackMode.UNTIL_DONE)
@@ -26,8 +30,14 @@ def on_button_pressed_b():
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
 basic.show_string("Self Test")
+music.play(music.tone_playable(698, music.beat(BeatFraction.WHOLE)),
+    music.PlaybackMode.UNTIL_DONE)
+basic.pause(1000)
+music.play(music.tone_playable(147, music.beat(BeatFraction.WHOLE)),
+    music.PlaybackMode.UNTIL_DONE)
+basic.pause(1000)
 pins.digital_write_pin(DigitalPin.P2, 1)
-basic.pause(2000)
+basic.pause(1000)
 pins.digital_write_pin(DigitalPin.P2, 0)
 basic.pause(1000)
 basic.show_string("Ready")
